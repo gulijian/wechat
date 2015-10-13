41a0371c2f847c0398ae1a4be6ce865171d4114c
gulj <1175327069@qq.com>
2015-10-13 17:16:35 +0800

    new add

--- ssmwechat/src/com/my/weixin/action/HomeAction.java
+++ ssmwechat/src/com/my/weixin/action/HomeAction.java
@@ -5,13 +5,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class HomePageAction {
+public class HomeAction {
 	
 	
-	@RequestMapping("/goHomePage")
+	@RequestMapping("/home")
 	public ModelAndView goHomePage(){
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("index");
+		mv.setViewName("home");
+		return mv;
+	}
+	
+	
+	@RequestMapping("/personal")
+	public ModelAndView goPersonal(){
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("personal");
 		return mv;
 	}
 	
